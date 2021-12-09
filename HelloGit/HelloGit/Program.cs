d185e1d11060801c86242d06e732bf81381f7dcd
Markus Zehetner <s2110438016@fhooe.at>
2021-12-09 11:33:32 +0100

    Changed greeting message

--- HelloGit/HelloGit/Program.cs
+++ HelloGit/HelloGit/Program.cs
@@ -6,7 +6,7 @@ namespace HelloGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Students!");
         }
     }
 }
